9d78ffcad667ce0a9891431794c6f6b8300e2040
Shun Git <kwokshungit@gmail.com>
2018-04-17 09:18:39 +0800

    add building and ground

--- js/site.js
+++ js/site.js
@@ -5,7 +5,7 @@ let lights = [[8, 4, 3], [-8, -4, 3]];
 
 class Site {
   constructor(scene) {
-    this.size = new THREE.Vector3(30, 15, 1);
+    this.size = new THREE.Vector3(90, 15, 1);
     this.bounds = new THREE.Box3();
 
     scene.add(new THREE.AmbientLight(0x333333));
@@ -28,6 +28,31 @@ class Site {
 
     this.resizeFloor();
     scene.add(this.floor);
+
+    this.buildingTexture = textureLoader.load('data/building.png');
+    this.buildingTexture.wrapS = THREE.RepeatWrapping;
+    this.buildingTexture.premultiplyAlpha = true;
+    this.buildingTexture.repeat.set(15, 1);
+    this.buildingTexture.anisotropy = 8;
+
+    this.building = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(135, 9),
+      new THREE.MeshPhongMaterial({color: 0xffffff, map: this.buildingTexture})
+    );
+
+    this.building.rotation.x += Math.PI / 2;
+    this.building.position.y += 6;
+    this.building.position.z += 4.5;
+
+    scene.add(this.building);
+
+    let ground = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(500, 500),
+      new THREE.MeshBasicMaterial({color: 0x0f0f0f})
+    );
+
+    ground.position.z = -0.01;
+    scene.add(ground);
 
     scene.background = new THREE.Color(0x0f0f18);
   }
@@ -59,5 +84,5 @@ class Site {
 
 var siteParams = {
   width: [10, 20, 1],
-  length: [20, 40, 1]
+  length: [20, 90, 1]
 }
